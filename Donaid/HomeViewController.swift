30bcba971c4ffdd7f09e83275170af6f2bd54d74
Alejandro Mendoza <amendoza@g.hmc.edu>
2017-05-15 20:50:08 -0700

    Added ability to expand sections.

--- Donaid/HomeViewController.swift
+++ Donaid/HomeViewController.swift
@@ -12,13 +12,13 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var tableView: UITableView!
     
+    
     var sectionsExpanded = [Int: Bool] ()
     var splitProjects = [Int: [HDXProject]]()
-    var sectionTitles = ["Syria Crisis", "CAR crisis", "South Sudan Crisis","Ebola Outbreak","Boko Haram Crisis","El Nino","Burundi Crisis","Horn of Africa Drought", "Haiti Cholera"]
-    
     var fullProjects: [HDXProject] = []
-    var dictProjects: [String: [HDXProject]] = [:]
-
+    var projectsByGrouping: [String: [HDXProject]] = [:]
+    var sectionTitles = ["Syria Crisis", "CAR Crisis", "South Sudan Crisis","Ebola Outbreak","Boko Haram Crisis","El Nino","Burundi Crisis","Horn of Africa Drought", "Haiti Cholera"]
+    var groupingNames = ["Syria crisis 2011-2016", "CAR crisis 2013-2016", "South Sudan crisis 2013-2016","Ebola outbreak 2014-2015","Boko Haram crisis 2014-2016","El Nino 2015-2016","Burundi crisis 2015-2016","Horn of Africa Drought 2017", "Haiti Cholera"]
     
     
     override func viewDidLoad() {
@@ -30,9 +30,15 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 140
         
+        initSections()
         fetchAllProjects()
         
-        print("made it fam")
+    }
+    
+    func initSections() {
+        for (index, _) in sectionTitles.enumerated() {
+            sectionsExpanded[index] = false
+        }
     }
     
     func fetchAllProjects() {
@@ -60,14 +66,15 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     func initProjects() {
         for project in fullProjects {
             
-            if let name = project.groupingName {
-                if dictProjects[name] == nil {
-                    dictProjects[name] = []
+            if let group = project.groupingName {
+                if projectsByGrouping[group] == nil {
+                    projectsByGrouping[group] = []
                 }
                 
-                dictProjects[name]?.append(project)
+                projectsByGrouping[group]?.append(project)
             }
         }
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,26 +83,104 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 8
+        return sectionTitles.count
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if projectsByGrouping.count > 0 {
+            let groupingProjects = getProjectsBySection(section: section)
+            return groupingProjects.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if sectionsExpanded[indexPath.section]! {
+            return UITableViewAutomaticDimension
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+   
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
-        
+        if projectsByGrouping.count > 0 {
+            let groupingProjects = getProjectsBySection(section: indexPath.section)
+            if groupingProjects.count > 0 {
+                if let project = groupingProjects[indexPath.row] as HDXProject? {
+                    cell.project = project
+                }
+            }
+            
+        }
+
         return cell
+    }
+    
+    func test(sender: UITapGestureRecognizer){
+        //using sender, we can get the point in respect to the table view
+        let view = sender.view
+        let section = view?.tag as Int!
+        
+        let tapLocation = sender.location(in: self.tableView)
+        
+        //using the tapLocation, we retrieve the corresponding indexPath
+        let indexPath = self.tableView.indexPathForRow(at: tapLocation)
+        
+        //finally, we print out the value
+        print(view?.tag)
+        
+        if sectionsExpanded[section!]! {
+            sectionsExpanded[section!] = false
+        } else {
+            sectionsExpanded[section!] = true
+        }
+        
+        tableView.reloadSections(IndexSet(integer: section!), with: .automatic)
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
     }
     
-
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //let headerView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 60))
+        let headerView = GroupHeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 60))
+        headerView.caption = sectionTitles[section]
+        headerView.tag = section
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.test))
+        headerView.addGestureRecognizer(gesture)
+        return headerView
+    }
+    
+    func getProjectsBySection(section: Int) -> [HDXProject] {
+        if (projectsByGrouping.count > 0) {
+            let groupingToGet = groupingNames[section]
+            if let groupingProjects = projectsByGrouping[groupingToGet] as [HDXProject]? {
+                return groupingProjects
+            } else {
+                return []
+            }
+            
+        } else {
+            return []
+        }
+        
+    }
+    
     
 
     /*
@@ -109,39 +194,4 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     */
 
     
-    //        HDXClient.sharedInstance?.getHDXProjectByYear(year: <#T##String#>, success: <#T##([HDXProject]) -> ()#>, failure: <#T##(Error) -> ()#>)(appId: appIdString, success: { (projects: [HDXProject]) in
-    //
-    //            switch appId {
-    //                case 497:
-    //                    self.syriaCrisisProjects = projects
-    //                    break
-    //                case 9:
-    //                    self.carCrisisProjects = projects
-    //                    break
-    //                case 10:
-    //                    self.southSudanProjects = projects
-    //                    break
-    //                case 526:
-    //                    self.ebolaOutbreakProjects = projects
-    //                    break
-    //                case 13:
-    //                    self.syriaCrisisProjects = projects
-    //                    break
-    //                case 14:
-    //                    self.syriaCrisisProjects = projects
-    //                    break
-    //                case 15:
-    //                    self.syriaCrisisProjects = projects
-    //                    break
-    //                case 16:
-    //                    self.syriaCrisisProjects = projects
-    //                    break
-    //                default:
-    //                    return
-    //            }
-    //
-    //            self.tableView.reloadData()
-    //        }) { (error: Error) in
-    //            print("HDX Client error")
-    //        }
 }
